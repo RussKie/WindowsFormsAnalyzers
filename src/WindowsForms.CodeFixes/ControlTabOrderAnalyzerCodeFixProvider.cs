473c15af8b1ee7c20215636a3fd7c32c96b63d4a
Igor Velikorossov <igor.velikorossov@microsoft.com>
2021-10-13 12:18:04 +1100

    Replace vars

--- src/WindowsForms.CodeFixes/ControlTabOrderAnalyzerCodeFixProvider.cs
+++ src/WindowsForms.CodeFixes/ControlTabOrderAnalyzerCodeFixProvider.cs
@@ -12,7 +12,9 @@ using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CodeRefactorings;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Options;
 using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
 using WindowsForms.Analyzers;
 
 namespace WindowsForms
@@ -33,14 +35,14 @@ namespace WindowsForms
 
         public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
-            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            SyntaxNode root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
             // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
-            var diagnostic = context.Diagnostics.First();
-            var diagnosticSpan = diagnostic.Location.SourceSpan;
+            Diagnostic diagnostic = context.Diagnostics.First();
+            TextSpan diagnosticSpan = diagnostic.Location.SourceSpan;
 
             // Find the type declaration identified by the diagnostic.
-            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
+            TypeDeclarationSyntax declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
 
             // Register a code action that will invoke the fix.
             context.RegisterCodeFix(
@@ -54,32 +56,32 @@ namespace WindowsForms
         private async Task<Solution> MakeUppercaseAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
         {
             // Compute new uppercase name.
-            var identifierToken = typeDecl.Identifier;
+            SyntaxToken identifierToken = typeDecl.Identifier;
             var newName = identifierToken.Text.ToUpperInvariant();
 
             // Get the symbol representing the type to be renamed.
-            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
-            var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
+            SemanticModel semanticModel = await document.GetSemanticModelAsync(cancellationToken);
+            INamedTypeSymbol typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
 
             // Produce a new solution that has all references to that type renamed, including the declaration.
-            var originalSolution = document.Project.Solution;
-            var optionSet = originalSolution.Workspace.Options;
-            var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
+            Solution originalSolution = document.Project.Solution;
+            OptionSet optionSet = originalSolution.Workspace.Options;
+            Solution newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
 
             // Return the new solution with the now-uppercase type name.
             return newSolution;
         }
     }
 
-    [ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = nameof(ControlTabOrderAnalyzerCodeRefactoringProvider)), Shared]
+    //[ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = nameof(ControlTabOrderAnalyzerCodeRefactoringProvider)), Shared]
 
-    public class ControlTabOrderAnalyzerCodeRefactoringProvider : CodeRefactoringProvider
-    {
-        public override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
-        {
-            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+    //public class ControlTabOrderAnalyzerCodeRefactoringProvider : CodeRefactoringProvider
+    //{
+    //    public override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
+    //    {
+    //        var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
-            throw new System.NotImplementedException();
-        }
-    }
+    //        throw new System.NotImplementedException();
+    //    }
+    //}
 }
